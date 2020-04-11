d763bcc8a0d9d669aca71a82d5d5b80a5a8e6e3d
Jubilee Young <workingjubilee@gmail.com>
2020-04-10 17:52:58 -0700

    Finish guessing game with variations
    Clippy warned about if_let_some_result redundancy, whoops.

--- rust/trpl/guessing_game/src/main.rs
+++ rust/trpl/guessing_game/src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{stdin};
+use std::cmp::Ordering::{Less, Greater, Equal};
+use rand::{Rng, thread_rng};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = thread_rng().gen_range(1, 101);
+    println!("Please input your guess.");
+
+    loop {
+        let mut guess = String::new();
+        stdin().read_line(&mut guess).expect("No read?");
+        if let Ok(guess) = guess.trim().parse::<u32>() {
+            println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+                Less => println!("Too small!"),
+                Greater => println!("Too big!"),
+                Equal => { println!("You win!"); break; }
+            }
+        } else {
+            continue;
+        }
+    }
 }
